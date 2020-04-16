4e6905fd14e8d17b97d18be547bc56608f7dac0e
caiofelix <cvff12345@gmail.com>
2020-04-16 13:36:51 -0300

    Criando activity login

---
+++ app/src/main/java/com/example/spending_control/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spending_control
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
